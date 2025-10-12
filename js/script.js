219fc207f4c60861f8b728dfd97ab5fcef09c8db
Maya <mnoren@umich.edu>
2025-10-12 14:49:35 -0400

    removed button

--- js/script.js
+++ js/script.js
@@ -4,16 +4,3 @@ if (document.getElementById('my-work-link')) {
   })
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-  document.querySelectorAll(".read-more-btn").forEach(btn => {
-    btn.addEventListener("click", () => {
-      const text = btn.previousElementSibling;
-      text.classList.toggle("expanded");
-      btn.textContent = text.classList.contains("expanded")
-        ? "Read less"
-        : "Read more";
-    });
-  });
-});
-
-
