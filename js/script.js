f8f2a6f47e666d219465b4a6d3ab2d591979b688
Maya <mnoren@umich.edu>
2025-10-12 14:18:07 -0400

    read more button

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll(".read-more-btn").forEach(btn => {
+      btn.addEventListener("click", () => {
+        const text = btn.previousElementSibling;
+        text.classList.toggle("expanded");
+        btn.textContent = text.classList.contains("expanded") ? "Read less" : "Read more";
+      });
+    });
+  });
